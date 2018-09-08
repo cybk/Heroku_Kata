d55fb6e87367565e6dd84357eebb3f6edbfc7561
Juan Miguel Alcala Suarez <cybk2000@hotmail.com>
2018-09-07 22:04:50 -0500

    removed references to localhost

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
 const PORT =  process.env.PORT ||  5000;
 
 const server = http.createServer((req, res ) => {
@@ -10,6 +9,6 @@ const server = http.createServer((req, res ) => {
 });
 
 
-server.listen(PORT, hostname, () => {
+server.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
 });
