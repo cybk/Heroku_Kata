abfde198df0b20f9eca75318537cc12ea10b0036
Juan Miguel Alcala Suarez <cybk2000@hotmail.com>
2018-09-08 15:38:06 -0500

    Updated according with eslint recomendations

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const http = require('http');
 
-const PORT =  process.env.PORT ||  5000;
+const PORT = process.env.PORT || 5000;
 
-const server = http.createServer((req, res ) => {
-    res.statusCode = 200;
-    res.setHeader('Content-type', 'text/plain');
-    res.end('Hello Heroku\n');
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-type', 'text/plain');
+  res.end('Hello Heroku\n');
 });
 
 
 server.listen(PORT, () => {
-    console.log(`Server running on port: ${PORT}`);
+  // eslint-disable-next-line
+   console.log(`Server running on port: ${PORT}`);
 });
